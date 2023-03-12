8777fc0a0b3b93e83358acb754031899ce12316d
Bohdan-Dubravin <dubravin111@gmail.com>
2023-03-12 10:46:10 +0200

    Clean pr

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default App;
